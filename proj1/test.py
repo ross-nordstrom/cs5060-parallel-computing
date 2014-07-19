2c6a13ed12fab97f29aa5071e5db78cc2e16925a
Ross Nordstrom <nordstrom.ross@gmail.com>
2014-07-19 17:04:10 -0600

    test script auto-generates expectations

--- proj1/test.py
+++ proj1/test.py
@@ -1,39 +1,48 @@
+import math
 import subprocess
+
+class Util:
+   @classmethod
+   def getExpectedLines(cls, proc, n):
+      expectedLines = list()
+      for rank in range(0, proc):
+         col = int(rank % n)
+         row = int(math.floor(rank / n))
+         up =    int( (rank-n)%proc )
+         down =  int( (rank+n)%proc )
+         left =  int( (row*n + (col-1)%n)%proc )
+         right = int( (row*n + (col+1)%n)%proc )
+         expectedLines.append("{}/POS ({},{})".format(rank, row, col))
+         expectedLines.append("{}/NBRS ({},{},{},{})".format(rank, up, down, left, right))
+         expectedLines.append("{}/INBUF ({},{},{},{})".format(rank, up, down, left, right))
+      return expectedLines
 
 print "Verify cartesian logic. Expect each process in cartesian topology to send its Rank to its neighbors"
 
-print "Test with 4 processors"
-expectedLines = [
-   "0/POS (0,0)",
-   "0/NBRS (2,2,1,1)",
-   "0/INBUF (2,2,1,1)",
-   "1/POS (0,1)",
-   "1/NBRS (3,3,0,0,",
-   "1/INBUF (3,3,0,0,",
-   "2/POS (1,0)",
-   "2/NBRS (0,0,3,3)",
-   "2/INBUF (0,0,3,3)",
-   "3/POS (1,1)",
-   "3/NBRS (1,1,2,2)",
-   "3/INBUF (1,1,2,2)",
-]
-cmd = ["mpirun", "-np", "4", "-f", "mpd.hosts", "./cart.o"]
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE,
-                          stdin=subprocess.PIPE)
-out, err = p.communicate()
-lines = out.split("\n")
-print "Test 4 proc output..."
-errCnt=0
-for line in expectedLines:
-   if line not in lines:
-      print "Error! Expected to find line: '{}'".format(line)
-      errCnt += 1
+for proc in [4, 9, 16]:
+   n = math.sqrt(proc)
+   print "Test with {} processors".format(proc)
+   expectedLines = Util.getExpectedLines(proc,n)
+   cmd = ["mpirun", "-np", str(proc), "-f", "mpd.hosts", "./cart.o"]
+   p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           stdin=subprocess.PIPE)
+   out, err = p.communicate()
+   lines = out.split("\n")
+   print "Test {} proc output...".format(proc)
+   errCnt=0
+   for line in expectedLines:
+      if line not in lines:
+         print "Error! Expected to find line: '{}'".format(line)
+         errCnt += 1
+      else:
+         lines.remove(line)
+   if errCnt==0:
+      print "Success!"
    else:
-      lines.remove(line)
-if errCnt==0:
-   print "Success!"
-else:
-   print "Error! Missing {} expected lines".format(errCnt)
-   print "Incorrect lines from program: "
-   print "\n".join(lines)
+      print "Error! Missing {} expected lines".format(errCnt)
+      print " "
+      print "Incorrect lines from program: "
+      print "\n".join(lines)
+
+
