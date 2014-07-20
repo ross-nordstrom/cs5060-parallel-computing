ae6288ba1f1053a04c570f90933d853c2ef8ce8f
Ross Nordstrom <nordstrom.ross@gmail.com>
2014-07-20 17:11:03 -0600

    test script for matrix mult

--- proj1/assignment2/test.py
+++ proj1/assignment2/test.py
@@ -1,48 +1,108 @@
 import math
 import subprocess
+import numpy      # sudo apt-get install python-numpy
+import os
 
 class Util:
    @classmethod
-   def getExpectedLines(cls, proc, n):
-      expectedLines = list()
-      for rank in range(0, proc):
-         col = int(rank % n)
-         row = int(math.floor(rank / n))
-         up =    int( (rank-n)%proc )
-         down =  int( (rank+n)%proc )
-         left =  int( (row*n + (col-1)%n)%proc )
-         right = int( (row*n + (col+1)%n)%proc )
-         expectedLines.append("{}/POS ({},{})".format(rank, row, col))
-         expectedLines.append("{}/NBRS ({},{},{},{})".format(rank, up, down, left, right))
-         expectedLines.append("{}/INBUF ({},{},{},{})".format(rank, up, down, left, right))
-      return expectedLines
+   def mmult(cls, a,b):
+      c = numpy.random.random_integers(0,0,(len(a),len(b)))
+      for i in range(len(a)):
+         for j in range(len(b[0])):
+            c[i][j] = 0
+            for k in range(len(b[0])):
+               c[i][j] += int(a[i][k] * b[k][j])
+      return c.astype(int)
+   @classmethod
+   def rand_matrix(cls, size):
+      m = numpy.random.random_integers(0,100, (size,size))
+      return m.astype(int)
+   @classmethod
+   def testmmult(cls,n,c):
+      # Run the program and check its output
+      cmd = ["mpirun","-np",str(n),"-f","mpd.hosts","./mmult.o"]
+      p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+      out, err = p.communicate()
 
-print "Verify cartesian logic. Expect each process in cartesian topology to send its Rank to its neighbors"
+      # Convert output into object
+      print "OUTPUT WAS:"
+      print out
+      outVal = [ map(int,row.split()) for row in out.split("\n") if(len(row.split()) > 0) ]
+      print "ACCURACY:"
+      print (c == outVal)
+      error = False in (c == outVal)
+      return error
 
-for proc in [4, 9, 16, 25, 36, 49, 64, 81, 100]:
-   n = math.sqrt(proc)
+
+
+print "Verify matrix multiplication"
+
+errorCnt = 0
+
+# Test sample
+a = [
+   [1,2,3,4],
+   [5,6,7,8],
+   [9,10,11,12],
+   [13,14,15,16]
+]
+b = [
+   [101,102,103,104],
+   [105,106,107,108],
+   [109,110,111,112],
+   [113,114,115,116]
+]
+c = Util.mmult(a,b)
+
+print a
+print "*"
+print b
+print "="
+print c
+print "-----------------------"
+
+if Util.testmmult(4,c):
+   errorCnt += 1
+
+# cmd = ["mv","data.txt","data.bak"]
+# p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+# out, err = p.communicate()
+
+for proc in []: #, 9, 16, 25, 36, 49, 64, 81, 100]:
+   n = proc
    print "Test with {} processors".format(proc)
-   expectedLines = Util.getExpectedLines(proc,n)
-   cmd = ["mpirun", "-np", str(proc), "-f", "mpd.hosts", "./cart.o"]
-   p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE,
-                           stdin=subprocess.PIPE)
-   out, err = p.communicate()
-   lines = out.split("\n")
-   print "Test {} proc output...".format(proc)
-   errCnt=0
-   for line in expectedLines:
-      if line not in lines:
-         print "Error! Expected to find line: '{}'".format(line)
-         errCnt += 1
-      else:
-         lines.remove(line)
-   if errCnt==0:
-      print "Success!"
-   else:
-      print "Error! Missing {} expected lines".format(errCnt)
-      print " "
-      print "Incorrect lines from program: "
-      print "\n".join(lines)
+   a = Util.rand_matrix(n)
+   b = Util.rand_matrix(n)
+   c = Util.mmult(a,b)
+
+   print a
+   print "*"
+   print b
+   print "="
+   print c
+   print "-----------------------"
+
+   # Setup data.txt input
+   fo = open("data.txt", "wb")
+   print "File name: "+fo.name
+   fo.write(str(n)+"\n")
+   fo.write(" \n")
+   for row in a:
+      fo.write( " ".join(map(str,row)) +"\n" )
+   fo.write(" \n")
+   for row in b:
+      fo.write( " ".join(map(str,row)) +"\n" )
+   fo.close()
+
+   if Util.testmmult(n,c):
+      errorCnt += 1
 
 
+# cmd = ["mv","data.bak","data.txt"]
+# p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+# out, err = p.communicate()
+
+if errorCnt == 0:
+   print "SUCCESS!"
+else:
+   print "AH! {} errors...".format(errorCnt)
