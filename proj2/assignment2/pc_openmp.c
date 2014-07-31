7a333500dc0224304f6277d3ab04bad85d9be08a
Ross Nordstrom <nordstrom.ross@gmail.com>
2014-07-30 22:12:15 -0600

    basic openmp producer consumer example working

--- proj2/assignment2/pc_openmp.c
+++ proj2/assignment2/pc_openmp.c
@@ -12,20 +12,41 @@
 
 char buffer = '\n';
 int taskAvailable = 0;
+int done = 0;
 
 int main(int argc, char* argv[]) {
   if(DBG) printf("Start program\n");
 
-  #pragma omp parallel num_threads (2) shared (buffer) shared (taskAvailable)
+  #pragma omp parallel num_threads (2) shared (buffer) shared (taskAvailable) shared (done)
   {
-    printf("[%ld] Entering parallel\n", pthread_self());
+    if(DBG) printf("[%ld] Entering parallel\n", pthread_self());
 
-    // Producer goes here
+    #pragma omp sections
+    {
+      #pragma omp section
+      {
+        // Producer goes here
+        if(DBG) printf("[%ld] Entering section -- Producer\n", pthread_self());
+        sleep(1);
+        done = 1;
+        if(DBG) printf("[%ld] Exiting section -- Producer\n", pthread_self());
+      } // omp section - producer
 
-    // Consumer goes here
+      #pragma omp section
+      {
+        // Consumer goes here
+        if(DBG) printf("[%ld] Entering section -- Consumer\n", pthread_self());
+        while(!done) {
+          if(DBG) printf("[%ld] Not done yet...\n", pthread_self());
+          usleep(100000);
+        }
+        if(DBG) printf("[%ld] Done.\n", pthread_self());
+        if(DBG) printf("[%ld] Exiting section -- Consumer\n", pthread_self());
+      } // omp section - consumer
 
-    printf("[%ld] Exiting parallel\n", pthread_self());
-  }
+    } // omp sections
+    if(DBG) printf("[%ld] Exiting parallel\n", pthread_self());
+  } // omp parallel
 
   if(DBG) printf("End program\n");
 }
