0b4ab95ff7b337e382672078ced4cf39ab207957
Ross Nordstrom <nordstrom.ross@gmail.com>
2014-07-30 22:32:52 -0600

    OpenMP producer-consumer working

--- proj2/assignment2/pc_openmp.c
+++ proj2/assignment2/pc_openmp.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#define DBG    1
+#define DBG    0
 #define DBG2   0
 
 char buffer = '\n';
@@ -17,36 +17,80 @@ int done = 0;
 int main(int argc, char* argv[]) {
   if(DBG) printf("Start program\n");
 
-  #pragma omp parallel num_threads (2) shared (buffer) shared (taskAvailable) shared (done)
+  #pragma omp parallel sections shared (buffer) shared (taskAvailable) shared (done)
   {
-    if(DBG) printf("[%ld] Entering parallel\n", pthread_self());
+    // if(DBG) printf("[%ld] Entering parallel\n", pthread_self());
 
-    #pragma omp sections
+    #pragma omp section
     {
-      #pragma omp section
+      // Producer goes here
+      if(DBG) printf("[%ld] Entering section -- Producer\n", pthread_self());
+
+      int i=0, maxStrSize = 1024*1024;  // Arbitrary max
+      FILE *fr = NULL;
+      char ch, line[1024*1024];
+
+      fr = fopen("string.txt", "rt");
+      if(DBG) printf("[%ld] File opened for reading.\n", pthread_self());
+
+      while(fgets(line, maxStrSize, fr) != NULL) {
+        if(DBG) printf("[%ld] Read line: '%s'\n", pthread_self(), line);
+
+        i = 0;
+        while( (ch = line[i]) != '\n' )
+        {
+          #pragma omp critical(taskQueue)
+          {
+            if(!taskAvailable)
+            {
+              buffer = ch;
+              taskAvailable = 1;
+              i++;
+            }
+          }
+        } // for each char in line
+      } // for each line in file
+      fclose(fr);
+
+      #pragma omp critical(isDone)
       {
-        // Producer goes here
-        if(DBG) printf("[%ld] Entering section -- Producer\n", pthread_self());
-        sleep(1);
         done = 1;
-        if(DBG) printf("[%ld] Exiting section -- Producer\n", pthread_self());
-      } // omp section - producer
+      }
 
-      #pragma omp section
-      {
-        // Consumer goes here
-        if(DBG) printf("[%ld] Entering section -- Consumer\n", pthread_self());
-        while(!done) {
-          if(DBG) printf("[%ld] Not done yet...\n", pthread_self());
-          usleep(100000);
+      if(DBG) printf("[%ld] Exiting section -- Producer\n", pthread_self());
+    } // omp section - producer
+
+    #pragma omp section
+    {
+      int imDone = 0;
+      char ch;
+
+      // Consumer goes here
+      if(DBG) printf("[%ld] Entering section -- Consumer\n", pthread_self());
+      while(!imDone) {
+        #pragma omp critical(taskQueue)
+        {
+          if(taskAvailable)
+          {
+            ch = buffer;
+            if(DBG) printf("[%ld] Got task from buffer: '%c'\n", pthread_self(), ch);
+            else printf("%c",ch);
+            taskAvailable = 0;
+          }
         }
-        if(DBG) printf("[%ld] Done.\n", pthread_self());
-        if(DBG) printf("[%ld] Exiting section -- Consumer\n", pthread_self());
-      } // omp section - consumer
 
-    } // omp sections
-    if(DBG) printf("[%ld] Exiting parallel\n", pthread_self());
-  } // omp parallel
+        #pragma omp critical(isDone)
+        {
+          imDone = done;
+        }
+      } // while i'm not done
+      if(DBG) printf("[%ld] Done.\n", pthread_self());
+      if(DBG) printf("[%ld] Exiting section -- Consumer\n", pthread_self());
+      else printf("\n");
+    } // omp section - consumer
+
+    // if(DBG) printf("[%ld] Exiting parallel\n", pthread_self());
+  } // omp parallel sections
 
   if(DBG) printf("End program\n");
 }
